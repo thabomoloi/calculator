7c74408a1d9545245115d59a0eea92b41d3bf791
Thabo Moloi <thabomoloicodes@gmail.com>
2022-07-22 21:16:11 +0200

    Add stack and queue class from programiz

--- scripts/script.js
+++ scripts/script.js
@@ -69,3 +69,87 @@ function operate(a, b, operator) {
             break;
     }
 }
+
+/**
+ * Queue implementation
+ * @link https://www.programiz.com/javascript/examples/queue
+ */
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    // add element to the queue
+    enqueue(element) {
+        return this.items.push(element);
+    }
+
+    // remove element from the queue
+    dequeue() {
+        if (this.items.length > 0) {
+            return this.items.shift();
+        }
+    }
+
+    // view the last element
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    // check if the queue is empty
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    // the size of the queue
+    size() {
+        return this.items.length;
+    }
+
+    // empty the queue
+    clear() {
+        this.items = [];
+    }
+}
+
+/**
+ * Stack implementation
+ * @link https://www.programiz.com/javascript/examples/stack
+ */
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    // add element to the stack
+    add(element) {
+        return this.items.push(element);
+    }
+
+    // remove element from the stack
+    remove() {
+        if (this.items.length > 0) {
+            return this.items.pop();
+        }
+    }
+
+    // view the last element
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    // check if the stack is empty
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    // the size of the stack
+    size() {
+        return this.items.length;
+    }
+
+    // empty the stack
+    clear() {
+        this.items = [];
+    }
+}
