4ef21c77b752ec2eed49e677b0be3ea9942e29cd
Thabo Moloi <thabomoloicodes@gmail.com>
2022-07-22 04:22:04 +0200

    Adds basic math functions

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+/**
+ * Adds two numbers.
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns the sum of a and b.
+ */
+function add(a, b) {
+    return a + b;
+}
+
+/**
+ * Subtract one number from the another.
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns the diffrence between a and b.
+ */
+function subtract(a, b) {
+    return a - b;
+}
+
+/**
+ * Multiplies two numbers.
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns the product of a and b.
+ */
+function multiply(a, b) {
+    return a * b;
+}
+
+/**
+ * Divide one number by another.
+ * @param {Number} a The dividend.
+ * @param {Number} b The divisor.
+ * @returns the quotient between a and b.
+ */
+function divide(a, b) {
+    return a / b;
+}
+
+/**
+ * Returns value of base taken to a specified number
+ * @param {Number} a The base.
+ * @param {Number} b The exponent.
+ */
+function pow(a, b) {
+    return a ** b;
+}
